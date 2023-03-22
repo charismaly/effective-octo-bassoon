562472b871ed2c1d8de46c2af555bb7c95c12417
Charisma Ly <112360145+charismaly@users.noreply.github.com>
2023-03-22 15:52:34 +0000

    Wed, Mar 22, 2023, 3:52 PM +00:00

--- js/counter.js
+++ js/counter.js
@@ -7,11 +7,15 @@ counters.forEach(counter => {
         const target = +counter.getAttribute('data-target')
         const c = +counter.innerText
 
-        const increment = target/200
+        const increment = target / 200
 
         if (c < target) {
             counter.innerText = `${Math.ceil(c + increment)}`
-            
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
         }
     }
+
+    updateCounter()
 })
