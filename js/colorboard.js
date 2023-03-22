2bbc374befb72ed3bf953690a1d1c0350c522b85
Charisma Ly <112360145+charismaly@users.noreply.github.com>
2023-03-22 17:03:13 +0000

    Wed, Mar 22, 2023, 5:03 PM +00:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -2,7 +2,7 @@ const container = document.getElementById('container')
 const colors = ['#a8e6cf', '#dcedc1', '#ffd3b6', '#ffaaa5', '#ff8b94']
 const SQUARES = 500
 
-for(let i = 0; i < SQUARES; i++) {
+for (let i = 0; i < SQUARES; i++) {
     const square = document.creatElement('div')
     square.classList.add('square')
     container.appendChild(square)
