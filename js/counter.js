e5ab2e433f0c97bf695ceb0c4b8fea664d41418f
Charisma Ly <112360145+charismaly@users.noreply.github.com>
2023-03-22 15:48:24 +0000

    Wed, Mar 22, 2023, 3:48 PM +00:00

--- js/counter.js
+++ js/counter.js
@@ -1,1 +1,17 @@
-cont counter = ocument.querySelectorAll('.counter')
+cont counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+
+        const increment = target/200
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            
+        }
+    }
+})
