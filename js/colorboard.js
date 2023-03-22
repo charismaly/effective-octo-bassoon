633c5c998e2951ed0d864bad9b49d7eb16049d0e
Charisma Ly <112360145+charismaly@users.noreply.github.com>
2023-03-22 16:29:55 +0000

    Wed, Mar 22, 2023, 4:29 PM +00:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -1,2 +1,7 @@
 const container = document.getElementById('container')
-const colors = []
+const colors = ['#a8e6cf', '#dcedc1', '#ffd3b6', '#ffaaa5', '#ff8b94']
+const SQUARES = 500
+
+for(let i = 0; i < SQUARES; i++) {
+    
+}
