f828a79bb1efda663d8a8a34ddb674c4c140628b
Charisma Ly <112360145+charismaly@users.noreply.github.com>
2023-03-22 16:41:40 +0000

    Wed, Mar 22, 2023, 4:41 PM +00:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -5,9 +5,14 @@ const SQUARES = 500
 for(let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
-    container.appendChild(square)
-    square.eddEventListener('mouseover', () => setColor(square))
+    container.appendChild('square')
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseout', () => removecolor(square))
 }
+
+function removeColor(element {
+    element.style.background = '#1d1d1d'
+})
 
 function setColor(element) {
     const color = getRandomColor()
